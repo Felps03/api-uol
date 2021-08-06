dcbe8d8e827e08b91602db263043e86566e97d49
Gabriel962008 <gabriel.alves962008@hotmail.com>
2021-08-05 22:25:03 -0300

    ajustes operações de insert update

--- config/routes.js
+++ config/routes.js
@@ -1,44 +1,16 @@
-//routes.js
 const express  = require('express');
 const route = express.Router();
 var dbCidades  = require('./dbCidades.json');
 var dbClientes = require('./dbClientes.json');
 
-// * Cadastrar cidade
-// * Cadastrar cliente
+// * Cadastrar cidade(ok)
+// * Cadastrar cliente(ok)
 // * Consultar cidade pelo nome (ok)
 // * Consultar cidade pelo estado (ok)
 // * Consultar cliente pelo nome (ok)
 // * Consultar cliente pelo Id (ok)
-// * Remover cliente
+// * Remover cliente(ok)
 // * Alterar o nome do cliente
-/*
-route.get('/cidade', (req,res)=>{
-	return res.json(dbCidades);
-});
-*//*
-route.get('/cidade/:nome', (req,res)=>{
-	var resultados = {};
-
-	if( req.params.nome ){
-		resultados = dbCidades.filter((el)=>{
-			return el.nome == req.params.nome;
-		});
-	}
-	res.send(resultados);
-});
-
-route.get('/cidade/estado/:estado', (req,res)=>{
-	var resultados = {};
-
-	if( req.params.estado ){
-		resultados = dbCidades.filter((el)=>{
-			return el.estado == req.params.estado;
-		});
-	}
-	res.send(resultados);
-});
-*/
 route.get(['/cidade', '/cidade/', '/cidade/:nome', '/cidade/estado/:estado'], (req,res)=>{
 	var resultados = {};
 
@@ -61,20 +33,14 @@ route.get(['/cidade', '/cidade/', '/cidade/:nome', '/cidade/estado/:estado'], (r
 	}	
 });
 
-/*
-route.get('/cliente', (req,res)=>{
-	res.send(dbClientes);
+route.post('/cidade', (req,res)=>{
+	var id = (dbCidades.length+1);
+	if( !req.body ){
+		return res.status(400).end();
+	}
+	dbCidades.push({id:id, nome:req.body.nome, estado:req.body.estado});
+	res.send(dbCidades);
 });
-*/
-/*
-route.get('/cliente/:id', (req,res)=>{
-	var resultados = dbClientes.filter((el)=>{
-		return el.id == req.params.id;
-	});
-	res.send(resultados);
-});
-*/
-
 
 route.get(['/cliente', '/cliente/', '/cliente/nome/:nome', '/cliente/:id'], (req,res)=>{
 	var resultados = {};
@@ -98,16 +64,32 @@ route.get(['/cliente', '/cliente/', '/cliente/nome/:nome', '/cliente/:id'], (req
 	}
 });
 
+route.post('/cliente', (req,res)=>{
+	console.log(req.body, dbClientes.length, (dbClientes.length+1));
+	var id = (dbClientes.length+1);
+
+	if( !req.body ){
+		return res.status(400).end();
+	}
+	dbClientes.push({id:id, nome:req.body.nome, sexo:req.body.sexo, data_nascimento:req.body.data_nascimento, idade:req.body.idade,cidade:req.body.cidade});
+	res.send(dbClientes);
+});
+
+route.put('/cliente/:id', (req,res)=>{
+	var index = dbClientes.findIndex((el)=>{ return el.id == req.params.id; });
+
+	if( !req.body ){
+		return res.status(400).end();
+	}
+
+	dbClientes[index].nome = req.body.nome;
+	res.send(dbClientes[index]);
+});
+
 route.delete('/cliente/:id', (req,res)=>{
-	var index = dbClientes.findIndex((el)=>{ if(el.id == req.params.id) return true; });
-	dbClientes.splice(cli.id,1);
-	res.send(index);
+	var index = dbClientes.findIndex((el)=>{ return el.id == req.params.id; });
+	dbClientes.splice(index,1);
+	res.send(dbClientes);
 });
 
-/*
-route.get('/cidade/estado/:estado', (req,res,next)=>{
-	res.send(req.params.estado);
-});
-
-*/
 module.exports = route;
